7442e58aa951a15785f8d6fc6d8b611f952f1aa2
Test <test@Tests-MacBook-Pro.local>
2024-12-08 22:55:07 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  /* toogle mesma logicado if/else abaixo para substituir o fundo */
+
+  // pegar a tag image
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+   if (html.classList.contains("light")) {
+    //se light mode, add imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // sem light mode, manter a normal
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
